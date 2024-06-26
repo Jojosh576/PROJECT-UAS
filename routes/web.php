e2babba2e1cd893e614bc8f985226301dc5f29ba
jamesnursalim <jmsboy23@gmail.com>
2024-06-26 10:39:14 +0700

    DB model

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,9 @@
 
 use App\Http\Controllers\TransactionController;
 
+Route::get('/', function(){
+    return view('test');
+});
+
 Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
 Route::post('/transactions', [TransactionController::class, 'store'])->name('transactions.store');
